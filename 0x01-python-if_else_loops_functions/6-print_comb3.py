293bcce2ae0d200fb9e75875441357caaeb16dd0
Mwahk3ymer <audreymwakima99@gmail.com>
2023-08-08 02:37:49 -0700

    diff combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        print(f"{i}{j}", end=", " if i != 8 or j != 9 else "\n")
+        print("{:02}".format(i * 10 + j), end=", " if i != 8 or j != 9 else "\n")
