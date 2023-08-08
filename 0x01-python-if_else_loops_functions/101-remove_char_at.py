4e742ceb728ab03366eae80e945e07e6ee8c72e0
Mwahk3ymer <audreymwakima99@gmail.com>
2023-08-08 04:43:44 -0700

    remove pos

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,10 +3,10 @@
 def remove_char_at(s, n):
     if n < 0 or n >= len(s):
         return s
-    
+
     new_string = ""
     for i in range(len(s)):
         if i != n:
             new_string += s[i]
-    
+ 
     return new_string
