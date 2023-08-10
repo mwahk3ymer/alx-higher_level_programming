ff6a591705ad93791d665a27686394c9b802d012
Mwahk3ymer <audreymwakima99@gmail.com>
2023-08-10 00:25:06 -0700

    imports func  does some Maths, and prints the result.

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    
+    sum_result = add(a, b)
+    diff_result = sub(a, b)
+    prod_result = mul(a, b)
+    div_result = div(a, b)
+    
+    print("{} + {} = {}".format(a, b, sum_result))
+    print("{} - {} = {}".format(a, b, diff_result))
+    print("{} * {} = {}".format(a, b, prod_result))
+    print("{} / {} = {}".format(a, b, div_result))
