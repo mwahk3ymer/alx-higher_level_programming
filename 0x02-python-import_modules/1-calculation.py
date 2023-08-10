7538d3108f16ab613994127b139c6c3859fd0685
Mwahk3ymer <audreymwakima99@gmail.com>
2023-08-10 01:52:34 -0700

    correct imp funcs from file calc

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,12 +3,12 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    
+
     sum_result = add(a, b)
     diff_result = sub(a, b)
     prod_result = mul(a, b)
     div_result = div(a, b)
-    
+
     print("{} + {} = {}".format(a, b, sum_result))
     print("{} - {} = {}".format(a, b, diff_result))
     print("{} * {} = {}".format(a, b, prod_result))
