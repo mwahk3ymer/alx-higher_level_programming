df6773f7f07956c06aa8a3af1f5d57e9583aa5ea
mwahk3ymer <audreymwakima99@gmail.com>
2023-08-28 14:29:28 +0300

    print wuith err

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
+from sys import exc_info, stderr
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except:
-        import sys
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        print("Exception: {}".format(exc_info()[1]), file=stderr)
         return False
