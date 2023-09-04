e5ed889cecb37a327a0ad0e2830afd1df8b3cfd4
mwahk3ymer <audreymwakima99@gmail.com>
2023-09-04 15:11:51 +0000

    correct

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -8,7 +8,7 @@ public area and perimeter methods, and allows printing #'s
 
 class Rectangle:
     """
-    This class defines a rectangle.
+    Defines class rectangle with private attribute width and height
 
     Args:
         width (int): width
@@ -24,40 +24,19 @@ class Rectangle:
         perimeter(self)
         __str__(self)
     """
-
     def __init__(self, width=0, height=0):
-        """
-        Initializes a rectangle with optional width and height.
-
-        Args:
-            width (int): The width of the rectangle. Default is 0.
-            height (int): The height of the rectangle. Default is 0.
-        """
+        """ Initialize rectangles """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """
-        Getter method for the width attribute.
-
-        Returns:
-            int: The width of the rectangle.
-        """
+        """ Getter returns width """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-        Setter method for the width attribute.
-
-        Args:
-            value (int): The new width value.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than 0.
-        """
+        """ Setter sets width if int > 0 """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -66,26 +45,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """
-        Getter method for the height attribute.
-
-        Returns:
-            int: The height of the rectangle.
-        """
+        """ Getter returns height """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        Setter method for the height attribute.
-
-        Args:
-            value (int): The new height value.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than 0.
-        """
+        """ Setter sets height if int > 0 """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -93,48 +58,18 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """
-        Calculate and return the area of the rectangle.
-
-        Returns:
-            int: The area of the rectangle.
-        """
+        """ Return width * height """
         return self.__width * self.__height
 
     def perimeter(self):
-        """
-        Calculate and return the perimeter of the rectangle.
-
-        Returns:
-            int: The perimeter of the rectangle.
-        """
+        """ Return 2*width + 2*height (or return 0 if width or height is 0)"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width + self.__height)
+        return (2 * self.__width) + (2 * self.height)
 
     def __str__(self):
-        """
-        Return a string representation of the rectangle.
-
-        Returns:
-            str: A string representation of the rectangle using '#'.
-        """
+        """ Prints rectangle with #'s """
         if self.__width == 0 or self.__height == 0:
             return ""
-
-        rectangle_str = ""
-        for i in range(self.__height):
-            for j in range(self.__width):
-                rectangle_str += "#"
-            if i < self.__height - 1:
-                rectangle_str += "\n"
-        return rectangle_str
-
-    def __repr__(self):
-        """
-        Return a string representation of the rectangle.
-
-        Returns:
-            str: A string representation of the rectangle.
-        """
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+        pic = "\n".join(["#" * self.__width for rows in range(self.__height)])
+        return pic
