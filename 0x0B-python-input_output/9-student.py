57419dd865b450d5f4a09f55fdfca713aaf082c4
Mwahk3ymer <audreymwakima99@gmail.com>
2023-09-12 05:46:45 -0700

    9student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
 """
-Contains the clas "Student"
+Contains the class "Student"
 """
 
 
 class Student:
-     """Representation of a student"""
-     def __init__(self, first_name, last_name, age):
-     """
-     Initializes a Student instanceame, and age.
-     """
-     self.first_name = first_name
-     self.last_name = last_name
-     self.age = age
+    """Representation of a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-     def to_json(self):
-     """
-     Retrieves a dictionary representation of a Student instance.
-     """
-     student_dict = {
-             'first_name': self.first_name,
-             'last_name': self.last_name,
-             'age': self.age
-             }
-     return student_dict
+    def to_json(self):
+        """returns a dictionary representation of a Student instance"""
+        return self.__dict__
