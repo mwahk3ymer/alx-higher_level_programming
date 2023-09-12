8eeb3e49b5602fdc64a6161bb199066369a7fc85
Mwahk3ymer <audreymwakima99@gmail.com>
2023-09-12 04:58:24 -0700

    commit

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -10,8 +10,5 @@ import json
 def to_json_string(my_obj):
     """
     Returns the JSON representation of an object as a string.
-
-    :param my_obj: The object to be converted to JSON.
-    :return: The JSON representation of the object as a string.
     """
     return json.dumps(my_obj)
