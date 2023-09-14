879c585140f8937d48e388a92e5710691d22d460
mwahk3ymer <audreymwakima99@gmail.com>
2023-09-14 12:08:21 +0000

    turtle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,6 +6,8 @@ This module defines the Base class.
 
 import json
 import csv
+import turtle
+
 
 class Base:
     """
@@ -176,3 +178,35 @@ class Base:
             return instances
         except FileNotFoundError:
             return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        # Initialize the Turtle graphics window
+        screen = turtle.Screen()
+        screen.title("Shapes Drawing")
+
+        # Create a Turtle object for drawing
+        t = turtle.Turtle()
+
+        # Draw rectangles
+        for rect in list_rectangles:
+            t.penup()
+            t.goto(rect.x, rect.y)
+            t.pendown()
+            for _ in range(2):
+                t.forward(rect.width)
+                t.left(90)
+                t.forward(rect.height)
+                t.left(90)
+
+        # Draw squares
+        for square in list_squares:
+            t.penup()
+            t.goto(square.x, square.y)
+            t.pendown()
+            for _ in range(4):
+                t.forward(square.size)
+                t.left(90)
+
+        # Close the Turtle graphics window on click
+        screen.exitonclick()
