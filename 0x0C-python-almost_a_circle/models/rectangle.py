efad419098b2aa6bb223e9f0c684012cbb776370
mwahk3ymer <audreymwakima99@gmail.com>
2023-09-14 07:47:31 +0000

    mproving the public method def display(self):

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -128,8 +128,10 @@ class Rectangle(Base):
         """
         Print the Rectangle instance using '#' characters to stdout.
         """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print('#' * self.__width)
+            print(' ' * self.__x + '#' * self.__width)
 
     def __str__(self):
         """
