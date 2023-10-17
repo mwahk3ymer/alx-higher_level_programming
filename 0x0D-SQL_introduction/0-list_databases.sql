e7c8ddf3c9f0b8360c53d221d1266e0aefd5467e
Mwahk3ymer <audreymwakima99@gmail.com>
2023-10-17 11:56:29 +0000

    cat 0-list_dat

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,2 +1,10 @@
-# Connect to the MySQL server
-SHOW DATABASES
+#!/bin/bash
+
+# This script lists all databases on a MySQL server
+
+# MySQL username and password
+MYSQL_USER="your_username"
+MYSQL_PASSWORD="your_password"
+
+# Use the mysql command to execute the query
+mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;"
