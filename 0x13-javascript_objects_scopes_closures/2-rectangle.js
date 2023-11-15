08348f57588b6f52e58b96972e21516cb5f88b15
Mwahk3ymer <audreymwakima99@gmail.com>
2023-11-15 07:29:33 +0000

    rect that defines rect

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,7 @@
 #!/usr/bin/node
 
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      // If w or h is not a positive integer, create an empty object
-      Object.create (null);
-    }
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 }
-
-module.exports = Rectangle;
