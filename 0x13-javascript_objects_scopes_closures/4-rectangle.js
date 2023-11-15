ee109dfcd39358b679e5ab45e7f32f1a6198186c
Mwahk3ymer <audreymwakima99@gmail.com>
2023-11-15 07:40:34 +0000

    rect#4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,32 +1,18 @@
 #!/usr/bin/node
-
-class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      // If w or h is not a positive integer, create an empty object
-      return Object.create(null);
-    }
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
-  print() {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
 
-  rotate() {
-    // Exchange the width and height
+  rotate () {
     [this.width, this.height] = [this.height, this.width];
   }
 
-  double() {
-    // Multiply the width and height by 2
-    this.width *= 2;
-    this.height *= 2;
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
   }
-}
-
-module.exports = Rectangle;
+};
