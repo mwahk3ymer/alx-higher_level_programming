944715bf7a3bb31fe89b705c48a75e1157bac9ff
Mwahk3ymer <audreymwakima99@gmail.com>
2023-11-15 07:25:46 +0000

    class Rect defines a rectangle:

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     } else {
       // If w or h is not a positive integer, create an empty object
-      Object.create(null);
+      Object.create (null);
     }
   }
 }
