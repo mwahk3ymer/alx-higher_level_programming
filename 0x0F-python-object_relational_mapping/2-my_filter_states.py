f43fafdc0993ee05f14d0b75b59924c191882212
Mwahk3ymer <audreymwakima99@gmail.com>
2023-11-24 09:05:40 +0000

    chmod

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,43 +1,19 @@
 #!/usr/bin/python3
-"""
-Script that displays all values in the states table of hbtn_0e_0_usa
-where name matches the argument.
-"""
+"""Lists states"""
 
 import MySQLdb
-import sys
-
-def search_states(username, password, db_name, state_name):
-    """
-    Display all values in the states table where name matches the argument.
-    """
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
-
-    cursor = db.cursor()
-
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(state_name)
-    cursor.execute(query)
-
-    rows = cursor.fetchall()
-
-    for row in rows:
-        print(row)
-
-    cursor.close()
-    db.close()
-
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <db_name> <state_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username, password, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-
-    search_states(username, password, db_name, state_name)
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    query = """
+SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
+    query = query.format(argv[4])
+    cur.execute(query)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
