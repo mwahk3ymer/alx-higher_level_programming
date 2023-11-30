f86d7033390e8db0b1f6da39b169a1c999a0b1b2
Mwahk3ymer <audreymwakima99@gmail.com>
2023-11-30 12:18:17 +0000

    3methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument and uses curl to display all HTTP methods the server will accept.
-curl -sI -X OPTIONS "$1" | grep -i "allow" | awk '{print $2}'
+#takes in a URL and displays all HTTP methods the server will accept
+curl -sI "$1" | grep "Allow" | cut -d " " -f2-
