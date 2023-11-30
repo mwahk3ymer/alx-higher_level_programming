b5053a5bf52e8288f644cc19c240af731eb223cc
Mwahk3ymer <audreymwakima99@gmail.com>
2023-11-30 12:15:43 +0000

    1-body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a GET request using curl, and displays the body of the response for a 200 status code.
-curl -s -X GET "$1" -w "%{http_code}" | awk 'END {if ($1 == "200") print $0}'
+# Takes in a URL, sends a GET request to the URL, displays the body of the response
+curl -sX GET "$1" -L 200
