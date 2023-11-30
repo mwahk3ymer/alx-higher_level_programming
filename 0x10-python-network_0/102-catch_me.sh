78d451a8017fb4a98f339e9c1d6e620a41ca3b4c
Mwahk3ymer <audreymwakima99@gmail.com>
2023-11-30 12:23:20 +0000

    102 try

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script makes a request to 0.0.0.0:5000/catch_me using curl to trigger the server response.
-curl -s -X PUT -L -d "user_id=98" 0.0.0.0:5000/catch_me
+# This script makes a request to 0.0.0.0:5000/catch_me causing the server to respond with "You got me!" in the body.
+curl -s -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
