44da06504775add0fe19ec990cc96e9c3b9a44ae
Mwahk3ymer <audreymwakima99@gmail.com>
2023-12-01 12:47:05 +0000

    chmod

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Script that uses GitHub API to list 10 commits of a repository by a user.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    # Check command-line arguments
+    if len(sys.argv) != 3:
+        print("Usage: {} <repository_name> <owner_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    # Set up the GitHub API URL for retrieving commits
+    api_url = f"https://api.github.com/repos/{owner_name}/{repository_name}/commits"
+
+    try:
+        # Send a GET request to the GitHub API to get the commits
+        response = requests.get(api_url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            commits = response.json()[:10]  # Take the first 10 commits
+            for commit in commits:
+                sha = commit['sha']
+                author_name = commit['commit']['author']['name']
+                print(f"{sha}: {author_name}")
+        else:
+            print("Error:", response.status_code, response.text)
+    except requests.RequestException as e:
+        print("Error:", e)
+        sys.exit(1)
