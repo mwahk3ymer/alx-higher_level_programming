fb73182974982e6405c091cc6ba0936514949db8
Mwahk3ymer <audreymwakima99@gmail.com>
2023-12-01 11:40:02 +0000

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module to fetch https://alx-intranet.hbtn.io/status
+"""
+
+from urllib import request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    
+    with request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body.decode('utf-8')))
